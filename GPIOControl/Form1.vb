eefd1cca7f56188bb2f7faf80d7cb82651dee27c
Max <max@derN3rd.de>
2016-07-22 15:37:51 +0200

    Initial

---
+++ GPIOControl/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Net
+Imports System.IO
+
+Public Class Form1
+    Dim gpioIPbox As Object
+    Dim gpioIP As String
+
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
+        Dim tempip As String
+        If gpioIP Is "" Or IsNothing(gpioIP) Then
+            tempip = "192.168.2.123"
+        Else
+            tempip = gpioIP
+        End If
+        gpioIPbox = InputBox("Bitte gebe die IP von RPi ein (z.B.: 192.168.2.123)", "GPIO Config", tempip)
+        If Not (gpioIPbox Is "") Then
+            GPIOControl.My.Settings.gpioIP = gpioIPbox
+            GPIOControl.My.Settings.Save()
+            gpioIP = gpioIPbox.ToString
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        If Not (GPIOControl.My.Settings.gpioIP Is "" Or IsNothing(GPIOControl.My.Settings.gpioIP)) Then
+            gpioIP = GPIOControl.My.Settings.gpioIP
+        End If
+    End Sub
+
+    Public Function doGpioRequest(device As Integer, status As Integer) As Integer
+        Try
+            Dim request As HttpWebRequest = HttpWebRequest.Create("http://" + gpioIP + "/" + device.ToString + "/" + status.ToString)
+            request.Method = "GET"
+
+            request.BeginGetResponse(New AsyncCallback(AddressOf ResponseRecent), request)
+            Return 1
+        Catch ex As Exception
+            Label1.Text = "Oh, da lief was schief"
+            Return 0
+        End Try
+        
+    End Function
+
+    Private Sub ResponseRecent(ByVal asynchronousResult As IAsyncResult)
+        Try
+            Dim webRequest As HttpWebRequest = DirectCast(asynchronousResult.AsyncState, HttpWebRequest)
+            Dim webResponse As HttpWebResponse = webRequest.EndGetResponse(asynchronousResult)
+            Dim stream As New StreamReader(webResponse.GetResponseStream())
+            Dim responseString = stream.ReadToEnd
+            Label1.Text = ""
+        Catch ex As Exception
+            Label1.Text = "Oh, da lief was schief"
+        End Try
+        
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        doGpioRequest(1, 1)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        doGpioRequest(1, 0)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        doGpioRequest(2, 1)
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        doGpioRequest(2, 0)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        doGpioRequest(3, 1)
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        doGpioRequest(3, 0)
+    End Sub
+End Class
---
+++ GPIOControl/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("GPIOControl")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("derN3rd.de")> 
+<Assembly: AssemblyProduct("GPIOControl")> 
+<Assembly: AssemblyCopyright("Copyright © derN3rd.de 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("a9d424a8-8a13-49c7-ab3a-0f595d6398f8")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
